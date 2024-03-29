04528d799f8d2d7589dd3e5f7342d507ec05829f
zhebom <zhebom@gmail.com>
2024-03-30 05:14:33 +0700

    firt-commit

---
+++ tests/Feature/EnvironmentTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+use function PHPUnit\Framework\assertEquals;
+
+class EnvironmentTest extends TestCase
+{
+    $youtube = env("YOUTUBE");
+   
+}
